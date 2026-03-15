d6d43d4c3a4459c0545eff64275e237dd2999078
NanaBananaC <55865865+NanaBananaC@users.noreply.github.com>
2026-03-15 16:16:13 +0000

    Deploying to gh-pages from @ NanaBananaC/nanabananac.github.io@883624d03268bd892c94db0edbb278e033bf6efd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,8 +451,8 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-speakez-an-innovative-hearing-assistive-prototype",
+          title: 'SpeakEZ - An Innovative Hearing-Assistive Prototype',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
